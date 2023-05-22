f43b3acf1f917aa73a975a80a5fc4c803721146b
mrivasc <00015121@uca.edu.sv>
2023-05-22 14:43:19 -0600

    :heavy_plus_sign: add: model

---
+++ MarineAnimalTracker/app/src/main/java/com/mrivasc/marineanimaltracker/data/models/MarineAnimalModel.kt
@@ -0,0 +1,6 @@
+package com.mrivasc.marineanimaltracker.data.models
+
+data class MarineAnimalModel(
+    val specie: String,
+    val habitat: String
+)
---
+++ MarineAnimalTracker/app/src/main/java/com/mrivasc/marineanimaltracker/data/models/MutableList.kt
@@ -0,0 +1,9 @@
+package com.mrivasc.marineanimaltracker.data.models
+
+// Dummy data
+val specie = "Beluga Whale"
+val habitat = "Arctic Ocean and its nearby seas in the Northern Hemisphere."
+
+val animals = mutableListOf(
+    MarineAnimalModel(specie, habitat)
+)
